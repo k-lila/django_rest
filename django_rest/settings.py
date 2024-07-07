dc419dd20a300818b83e54a203e68ccc4e9da5fd
k-lila <krishnalilapalandi@gmail.com>
2024-07-06 23:26:00 -0300

    settings => allowed hosts

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -158,4 +158,4 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["ebac-bookstore-api.onrender.com", "localhost", "0.0.0.0", "127.0.0.1"]
